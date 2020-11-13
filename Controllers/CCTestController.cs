e1ea1e3df35bef0e8fcaebc48b159e1df710807d
juntao.chen <juntao.chen@DESKTOP-B1DFB77>
2020-11-13 18:24:19 +0800

    测试提交

---
+++ Controllers/CCTestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XyProject.Controllers
+{
+    [Route("api/CCTest/[action]")]
+    [ApiController]
+    public class CCTestController : ControllerBase
+    {
+       
+        [HttpGet]
+        public void add(int id)
+        {
+            //测试提交
+
+           
+        }
+    }
+}
