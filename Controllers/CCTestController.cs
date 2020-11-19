4bc1bb2f6eb77c4fa2a6e9a595618b00ed04f93a
juntao.chen <juntao.chen@DESKTOP-B1DFB77>
2020-11-19 18:57:29 +0800

    添加 内存缓存

--- Controllers/CCTestController.cs
+++ Controllers/CCTestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CTCache.interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,13 +12,23 @@ namespace XyProject.Controllers
     [ApiController]
     public class CCTestController : ControllerBase
     {
-       
+        private readonly CacheTarget _cache;
+
+        public CCTestController(CacheTarget cache)
+        {
+            _cache = cache;
+        }
+
+
         [HttpGet]
-        public void add(int id)
+        public object add(int id)
         {
             //测试提交
+            _cache.Add("hellowword", "一起看世界");
 
-           
+            _cache.Replace("hellowword", "一起看世界22",new TimeSpan(0,10,0));
+
+            return _cache.Get("hellowword");
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CTCache.interfaces;
+using CTCache.Servier;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +27,12 @@ namespace XyProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //接口注入
+            services.AddScoped<IMemoryCacheHelper, MemoryCacheHelper>();
+            services.AddScoped<IRedisHelper, RedisHelper>();
+            services.AddScoped<CacheTarget, CacheAdapter>();
+
+            services.AddMemoryCache();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
